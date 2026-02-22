14640b2b3de4b46a4c7327a7a51c23ca19b67d0e
mukund2000 <mukundrastogixyz@gmail.com>
2026-02-23 00:08:25 +0530

    Added a tool to check availability of doctor based on specialisation

--- main.py
+++ main.py
@@ -46,16 +46,48 @@ def check_availability_by_doctor(desired_date:DateModel, doctor_name:Literal['ke
 
     return output
 
-llm_with_tools = llm.bind_tools([check_availability_by_doctor])
+@tool
+def check_availability_by_specialization(desired_date:DateModel, specialization:Literal["general_dentist", "cosmetic_dentist", "prosthodontist", "pediatric_dentist","emergency_dentist","oral_surgeon","orthodontist"]):
+    """
+    Checking the database if we have availability for the specific specialization.
+    The parameters should be mentioned by the user in the query
+    """
+    df = pd.read_csv(r"C:\Users\mukun\OneDrive\Documents\Projects\doctor-appointment\project\data\doctor_availability.csv")
+    df['date_slot_time'] = df['date_slot'].apply(lambda input: input.split(' ')[-1])
+    rows = df[(df['date_slot'].apply(lambda input: input.split(' ')[0]) == desired_date.date) & (df['specialization'] == specialization) & (df['is_available'] == True)].groupby(['specialization', 'doctor_name'])['date_slot_time'].apply(list).reset_index(name='available_slots')
+    print("rows:",rows)
+    if len(rows) == 0:
+        output = "No availability in the entire day"
+    else:
+        def convert_to_am_pm(time_str):
+            # Split the time string into hours and minutes
+            time_str = str(time_str)
+            hours, minutes = map(int, time_str.split(":"))
+            
+            # Determine AM or PM
+            period = "AM" if hours < 12 else "PM"
+            
+            # Convert hours to 12-hour format
+            hours = hours % 12 or 12
+            
+            # Format the output
+            return f"{hours}:{minutes:02d} {period}"
+        output = f'This availability for {desired_date.date}\n'
+        for row in rows.values:
+            output += row[1] + ". Available slots: \n" + ', \n'.join([convert_to_am_pm(value)for value in row[2]])+'\n'
+
+    return output
+
+llm_with_tools = llm.bind_tools([check_availability_by_doctor, check_availability_by_specialization])
 
 response = llm_with_tools.invoke([
     SystemMessage(content="You are a medical assistant. If user asks about doctor availability, you MUST call the tool."),
-    HumanMessage(content="What are the available slots for doctor kevin anderson on 7-8-2024?")
+    HumanMessage(content="What are the available slots for general dentist on 7-8-2024?")
 ])
 print(response)
 
 
 if response.tool_calls:
     tool_call = response.tool_calls[0]
-    tool_result = check_availability_by_doctor.invoke(tool_call["args"])
+    tool_result = check_availability_by_specialization.invoke(tool_call["args"])
     print("Tool Output:", tool_result)
