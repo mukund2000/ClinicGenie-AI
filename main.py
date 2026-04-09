bbd8df4f6ae217ef0c162e455825fac35f4e70ed
mukund2000 <mukundrastogixyz@gmail.com>
2026-04-09 22:53:39 +0530

    Added a tool to reschedule appointment

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ load_dotenv()
 # Base LLM instance (no tools attached yet)
 llm=ChatGroq(model_name="openai/gpt-oss-20b")
 
-llm_with_tools = llm.bind_tools([check_availability_by_doctor, check_availability_by_specialization, set_appointment, cancel_appointment])
+llm_with_tools = llm.bind_tools([check_availability_by_doctor, check_availability_by_specialization, set_appointment, cancel_appointment, reschedule_appointment])
 
 response = llm_with_tools.invoke([
     SystemMessage(content="You are a medical assistant. If user asks about doctor availability, you MUST call the tool."),
-    HumanMessage(content="cancel my appointment with a dr. john doe on 5-8-2024 08:00? My id number is 1234567`")
+    HumanMessage(content="reschedule my appointment with a dr. john doe from 5-8-2024 08:00 to 7-8-2024 8:30? My id number is 1234567`")
 ])
 print(response)
 
 
 if response.tool_calls:
     tool_call = response.tool_calls[0]
-    tool_result = cancel_appointment.invoke(tool_call["args"])
+    tool_result = reschedule_appointment.invoke(tool_call["args"])
     print("Tool Output:", tool_result)
--- toolkit/tools.py
+++ toolkit/tools.py
@@ -92,3 +92,20 @@ def cancel_appointment(date:DateTimeModel, id_number:IdentificationNumberModel, 
         df.to_csv(r"C:\Users\mukun\OneDrive\Documents\Projects\doctor-appointment\ClinicGenie-AI\data\availability.csv", index = False)
 
         return "Successfully cancelled"
+    
+
+@tool
+def reschedule_appointment(old_date:DateTimeModel, new_date:DateTimeModel, id_number:IdentificationNumberModel, doctor_name:Literal['kevin anderson','robert martinez','susan davis','daniel miller','sarah wilson','michael green','lisa brown','jane smith','emily johnson','john doe']):
+    """
+    Rescheduling an appointment.
+    The parameters MUST be mentioned by the user in the query.
+    """
+    #Dummy data
+    df = pd.read_csv(r"C:\Users\mukun\OneDrive\Documents\Projects\doctor-appointment\ClinicGenie-AI\data\availability.csv")
+    available_for_desired_date = df[(df['date_slot'] == new_date.date)&(df['is_available'] == True)&(df['doctor_name'] == doctor_name)]
+    if len(available_for_desired_date) == 0:
+        return "Not available slots in the desired period"
+    else:
+        cancel_appointment.invoke({'date':old_date, 'id_number':id_number, 'doctor_name':doctor_name})
+        set_appointment.invoke({'desired_date':new_date, 'id_number': id_number, 'doctor_name': doctor_name})
+        return "Successfully rescheduled for the desired time"
